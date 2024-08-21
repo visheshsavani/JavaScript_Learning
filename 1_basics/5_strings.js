824bf9959f448ef7304d929ee6aeca67d22a0716
visheshsavani <visheshsavani56@gmail.com>
2024-08-21 10:59:49 +0530

    strings_notes

---
+++ 1_basics/5_strings.js
@@ -0,0 +1,16 @@
+const student_name = "vishesh"
+console.log(student_name)
+let age = 20
+
+//the below syntax is very imp as it is now a days very popular
+console.log(`Hi myself ${student_name} and my age is ${age}`)
+
+let shopname = "gada_electronics"
+console.log(shopname[9]) 
+// basically we can access the character by indexing note that it is not a array
+
+let a = shopname.split('_');
+// the above is used to get a array from a string by any special separator
+console.log(a)
+
+
